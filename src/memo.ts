667f8875bd326fb3fcaf2c8c08b2dc57630edb5b
baglana <102351104+baglana@users.noreply.github.com>
2024-04-09 16:38:02 +0500

    Add hw1/{12,13}

--- src/memo.ts
+++ src/memo.ts
@@ -2,10 +2,42 @@ const memo = <T extends (...args: any[]) => any>(
   func: T,
   time?: number
 ): ((...arg: Parameters<T>) => ReturnType<T>) => {
-  if (typeof func !== 'function' || typeof time !== 'number' || time < 0) {
+  if (
+    typeof func !== 'function' ||
+    (typeof time !== 'undefined' && (typeof time !== 'number' || time < 0))
+  ) {
     throw new Error('INVALID_ARGUMENT');
   }
-  return func;
+  const cache = new Map();
+  const timeoutIdCache = new Map();
+  return function () {
+    const key = hash(arguments); // (*)
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    const result = func.call(this, ...arguments); // (**)
+
+    cache.set(key, result);
+
+    if (time) {
+      if (timeoutIdCache.has(key)) {
+        clearTimeout(timeoutIdCache.get(key));
+      }
+
+      const timeoutId = setTimeout(() => {
+        cache.delete(key);
+      }, time + 1);
+
+      timeoutIdCache.set(key, timeoutId);
+    }
+
+    return result;
+  };
 };
+
+function hash(args) {
+  return [].join.call(args);
+}
 
 export default memo;
--- src/promiseFrame.ts
+++ src/promiseFrame.ts
@@ -15,10 +15,51 @@ const promiseFrame = async <
   functions: T,
   limit?: number
 ): Promise<ResultsT[]> => {
-  if (!Array.isArray(functions) || typeof limit !== 'number' || limit <= 0) {
+  if (!Array.isArray(functions) || (limit !== undefined && limit <= 0)) {
     throw new Error('INVALID_ARGUMENT');
   }
-  return [];
+  return new Promise((resolve, reject) => {
+    const funcQueue: { index: number; func: FunctionType<any> }[] =
+      functions.map((func, index) => ({
+        index,
+        func,
+      }));
+
+    const results: ResultsT[] = [];
+    let executedCount: number = 0;
+
+    const executor = async ({
+      func,
+      index,
+    }: {
+      index: number;
+      func: FunctionType<any>;
+    }) => {
+      try {
+        results[index] = await func();
+        executedCount++;
+
+        if (executedCount === functions.length) {
+          resolve(results);
+        }
+
+        const curFuncObj = funcQueue.shift();
+        if (curFuncObj) {
+          executor(curFuncObj);
+        }
+      } catch (e) {
+        reject(e);
+      }
+    };
+
+    for (let i = 0; i < (limit || functions.length); i++) {
+      const funcObj = funcQueue.shift();
+
+      if (funcObj) {
+        executor(funcObj);
+      }
+    }
+  });
 };
 
 export default promiseFrame;
